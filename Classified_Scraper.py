3c064d584a1cd7502de8cbc13edbb1e0c4b5c53b
Hani Jafer <haanijessin@gmail.com>
2024-04-23 16:40:59 +0300

    Update Readme, Add requirements

--- Classified_Scraper.py
+++ Classified_Scraper.py
@@ -1,10 +1,10 @@
-import re, requests, time, sys, pyocr
+import re, requests, time, sys
 from datetime import date
 from bs4 import BeautifulSoup
 from wand.image import Image as wandImage
 from PIL import Image
-from pyocr.tesseract import image_to_string
-from io import StringIO
+import pytesseract
+
 
 def randomLoading():
     dots = " . .\n"
@@ -13,33 +13,34 @@ def randomLoading():
         sys.stdout.flush()
         time.sleep(0.1)
 
+
 sys.stdout.write("Starting Classfied Scraping .")
 randomLoading()
 
-#Getting link for latest PDF file
-today = (date.today()).strftime("%Y_%#m_%#d")
+# Getting link for latest PDF file
+today = date.today().strftime("%Y%m%d")
 mainPage = requests.get("https://www.gulf-times.com/Classified")
 content = mainPage.content
-soup = BeautifulSoup(content,"html.parser")
+soup = BeautifulSoup(content, "html.parser")
 link = soup.find(href=re.compile(today))["href"]
 
-#Downloading the PDF file from link
+# Downloading the PDF file from link
 sys.stdout.write("Downloading PDF .")
 randomLoading()
 
 myFile = requests.get(link)
-filePath = "C:/Users/haani/source/repos/Classified Scraper/PDFDownloads/classified.pdf"
+filePath = "./PDFDownloads/classified.pdf"
 with open(filePath, "wb") as PDF:
     PDF.write(myFile.content)
 
-#OCR and checking for required data
-sys.stdout.write("Checking Data .")
+# OCR and checking for required data
+sys.stdout.write("Checking Data.")
 randomLoading()
 
-#For wand to work, install ImageMagik, GhostScript for windows.
-#For OCR to work, install Tesseracr and add to path.
+# For wand to work, install ImageMagik, GhostScript for windows.
+# For OCR to work, install Tesseract OCR and add to path.
 
-#def dataCollect(filePath):
+# def dataCollect(filePath):
 #    imgPath = "C:/Users/haani/source/repos/Classified Scraper/PDFDownloads/image.jpg"
 #    dataPath = "C:/Users/haani/source/repos/Classified Scraper/PDFDownloads/data.txt"
 
@@ -55,18 +56,21 @@ randomLoading()
 #                         data+=line
 #    return data
 
+
 def datacollect(filepath):
     with wandImage(filename=filepath, resolution=300) as pdfimage:
         for i in pdfimage.sequence:
             img = wandImage(image=i)
-            imgpath = "c:/users/haani/source/repos/classified scraper/pdfdownloads/image.jpg"
+            imgpath = "./PDFDownloads/image.jpg"
             img.save(filename=imgpath)
-            initData = image_to_string(Image.open(imgpath))
+            initData = pytesseract.image_to_string(Image.open(imgpath))
+
             data = """"""
             for line in initData:
-                if not 'situation wanted' in line.lower():
-                    data+=line
+                if not "situation wanted" in line.lower():
+                    data += line
                 else:
                     return data
 
-print(datacollect(filePath))
+
+datacollect(filePath)
